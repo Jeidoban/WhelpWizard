e52023dfd50d229a397593f93c45c6e607e67d36
Joafie <jade424433@gmail.com>
2017-07-03 23:17:56 -0700

    Added more to the UI

--- WhelpWizard/Calculator.xaml.cs
+++ WhelpWizard/Calculator.xaml.cs
@@ -11,11 +11,44 @@ namespace WhelpWizard
         {
             InitializeComponent();
             calculatedDate.Text = DateTime.Today.ToString("ddd, MMM d, yyyy");
+            pregnancyInfo.Text = PregnancyInfo.firstStage;
+            stepper.Value = 1;
+            stepper.Minimum = 1;
+            stepper.Maximum = 6;
+            stepper.Increment = 1;
+            timeSpan.Text = DateTime.Today.ToString("ddd, MMM d, yyyy") + " - " + CalculateDate.NumberOfDays(DateTime.Today, 14);
         }
 
         void Handle_DateSelected(object sender, Xamarin.Forms.DateChangedEventArgs e)
         {
             calculatedDate.Text = CalculateDate.NumberOfDays(picker.Date, 52);
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            if ((int)stepper.Value == 1) 
+            {
+                pregnancyInfo.Text = PregnancyInfo.firstStage;
+            } else if ((int)stepper.Value == 2)
+            {
+                pregnancyInfo.Text = PregnancyInfo.secondStage;
+            }
+			else if ((int)stepper.Value == 3)
+			{
+                pregnancyInfo.Text = PregnancyInfo.thirdStage;
+			}
+			else if ((int)stepper.Value == 4)
+			{
+                pregnancyInfo.Text = PregnancyInfo.fourthStage;
+			}
+			else if ((int)stepper.Value == 5)
+			{
+                pregnancyInfo.Text = PregnancyInfo.fifthStage;
+			}
+			else if ((int)stepper.Value == 6)
+			{
+                pregnancyInfo.Text = PregnancyInfo.sixthStage;
+			}
         }
     }
 }
--- WhelpWizard/PregnancyInfo.cs
+++ WhelpWizard/PregnancyInfo.cs
@@ -3,10 +3,29 @@ namespace WhelpWizard
 {
     public class PregnancyInfo
     {
-        public PregnancyInfo()
-        {
-        }
+        public static string firstStage = "• Eggs are fertilized and aren't easily \n  affected by outside factors.\n" +
+										  "• Morning sickness may occur.\n" +
+										  "• No change in feeding or exercise.\n" +
+										  "• No medications unless approves \n  during pregnancy.\n" +
+										  "• Do not use flea or other chemical \n  treatments unless approved for \n  pregnancy.\n" +
+										  "• Do not vaccinate during pregnancy.\n";
+        
+        public static string secondStage = "• Embryo implants in uterus.\n";
 
+        public static string thirdStage = "• This is the time when embryos are \n  the most susceptible to outside \n  factors.\n" +
+                                          "• Nipples may begin to enlarge and \n  swell.\n";
+        
+		public static string fourthStage = "• Embryos will resist outside factors \n  more easily.\n" +
+										   "• Bitch's abdomen begins to swell and \n  weight slowly increases.\n" +
+										   "• Increase food by 1/4 the normal \n  amount.\n" +
+										   "• Ultrasound can detect the presence \n  of fetuses at this stage.\n";
+        
+		public static string fifthStage = "• Nipples continue to enlarge and \n  abdominal swelling becomes more \n   pronounced.\n" +
+										  "• Continue increasing food to about \n  two to three times the normal amount.\n" +
+										  "• Do not allow any vigorous play or \n  jumping.\n";
 
+		public static string sixthStage = "• Puppy movement can be felt when \n  bitch is still\n" +
+										  "• Bitch may begin nesting activity.\n" +
+										  "• Appetite will increase at the \n  beginning of this phase, then slowly \n  decline as due date approaches.";
     }
 }
