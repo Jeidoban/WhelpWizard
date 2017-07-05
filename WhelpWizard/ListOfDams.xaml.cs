ad3449781c72974e519163a6628cb92e3ccaa191
Joafie <jade424433@gmail.com>
2017-07-05 00:06:39 -0700

    Figured out how to get information from a cell.

--- WhelpWizard/ListOfDams.xaml.cs
+++ WhelpWizard/ListOfDams.xaml.cs
@@ -15,6 +15,7 @@ namespace WhelpWizard
             InitializeComponent();
             this.dog = dog;
             damsList.ItemsSource = dog; // IMPORTANT! The listview in the XAML has to be connected to a dams list.
+            //thing();
         }
 
         public ListOfDams()
@@ -25,6 +26,25 @@ namespace WhelpWizard
         public void addDog(Dog dog)
         {
             this.dog.Add(dog);   
+        }
+
+        void OnMore(object sender, System.EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+			DisplayAlert("More Context Action", mi.CommandParameter + " more context action", "OK");
+        }
+
+		void OnDelete(object sender, System.EventArgs e)
+		{
+            var mi = ((MenuItem)sender);
+            DisplayAlert("Delete Context Action", mi.CommandParameter + " delete context action", "OK");
+        }
+
+        void Handle_ItemTapped(object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            //string thing = ((Dog)e.Item).DogName;
+            //DisplayAlert("titld", thing, "ok");
+            Navigation.PushAsync(new DamInformation());
         }
     }
 }
